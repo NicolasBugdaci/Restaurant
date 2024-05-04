de4e4b814a12b93e740556dae8006c709fb29aa4
CheopsCode <nicolas.bugdaci@icloud.com>
2024-05-04 20:46:16 +0200

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Nicolas Bugdaci on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
